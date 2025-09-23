f65635f128aab95a7495e937b1151ab8fdea3e93
lsh9511 <iamshopsh@naver.com>
2025-09-23 15:12:45 +0900

    💡 chore : signup/ request 스키마 변경(password2 삭제)

--- app/features/users/router.py
+++ app/features/users/router.py
@@ -66,7 +66,6 @@ async def signup(request: SignupRequest):
     user = await User.create(
         email=request.email,
         password=hashed_password,
-        password2=hashed_password,
         phone_number=request.phone_number,
         nickname=request.nickname,
         user_type=request.user_type,
